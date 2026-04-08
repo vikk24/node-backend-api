dca34043ef7b641b5d03369ac4c76de3f8c7d0f5
Vinay Tiwari <vinaytiwari.rd@gmail.com>
2026-04-08 12:58:32 +0530

    Added cloud DB config

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,39 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('❌ DB connection failed:', err);
+//     } else {
+//         console.log('✅ MySQL Connected');
+//     }
+// });
+
+// module.exports = db;
+
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // 👈 add this
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('❌ DB connection failed:', err);
-    } else {
-        console.log('✅ MySQL Connected');
-    }
+  if (err) {
+    console.log("❌ DB connection failed:", err);
+  } else {
+    console.log("✅ DB connected");
+  }
 });
 
 module.exports = db;
